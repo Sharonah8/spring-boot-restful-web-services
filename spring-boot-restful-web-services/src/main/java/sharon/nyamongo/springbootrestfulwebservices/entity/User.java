dcb72956dc32b7f02a8f6682cb8949e710dc939c
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-12 19:59:54 +0300

    Initial commit

---
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/entity/User.java
@@ -0,0 +1,27 @@
+package sharon.nyamongo.springbootrestfulwebservices.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Getter
+@Setter
+@NoArgsConstructor
+// for parameterized constructors use AllArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ spring-boot-restful-web-services/src/test/java/sharon/nyamongo/springbootrestfulwebservices/SpringBootRestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package sharon.nyamongo.springbootrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
