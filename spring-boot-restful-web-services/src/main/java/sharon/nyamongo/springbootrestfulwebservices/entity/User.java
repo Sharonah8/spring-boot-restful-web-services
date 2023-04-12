801796088e3e85509493383b96d297085a4e8cc2
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-12 20:07:50 +0300

    Create User JPA Entity

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/entity/User.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/entity/User.java
@@ -22,6 +22,9 @@ public class User {
 
     @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private String email;
 }
