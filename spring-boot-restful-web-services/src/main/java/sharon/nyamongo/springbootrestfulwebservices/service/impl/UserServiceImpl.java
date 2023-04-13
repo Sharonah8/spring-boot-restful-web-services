e7b875e58eca19f25ac13a5b27bba20145b0877a
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-13 11:34:40 +0300

    Implement getUserById() method in UserServiceImpl, to return an optional of User

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/service/impl/UserServiceImpl.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/service/impl/UserServiceImpl.java
@@ -6,6 +6,8 @@ import sharon.nyamongo.springbootrestfulwebservices.entity.User;
 import sharon.nyamongo.springbootrestfulwebservices.repository.UserRepository;
 import sharon.nyamongo.springbootrestfulwebservices.service.UserService;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -14,5 +16,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public User createUser(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public User getUserById(Long userId) {
+        Optional<User> optionalUser = userRepository.findById(userId);
+        return optionalUser.get();
     }
 }
