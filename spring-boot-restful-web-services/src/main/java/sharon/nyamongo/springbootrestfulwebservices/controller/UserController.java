3f2cf34a050b2b217f24b7465f2bc38ca6edf1a6
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-13 11:26:37 +0300

    Build Create User REST API, add base URL for Postman testing

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/controller/UserController.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/controller/UserController.java
@@ -4,18 +4,23 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sharon.nyamongo.springbootrestfulwebservices.entity.User;
 import sharon.nyamongo.springbootrestfulwebservices.service.UserService;
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("api/users")
 public class UserController {
 
     private UserService userService;
 
     // build create User REST API
-    public ResponseEntity<User> createUser(User user){
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
        User savedUser = userService.createUser(user);
        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
