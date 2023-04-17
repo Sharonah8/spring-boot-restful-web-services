6038ee7832ff2105e00e7b55ea4bb19644e410cd
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-17 11:20:41 +0300

    Configure ModelMapper class as a Spring Bean

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/SpringBootRestfulWebServicesApplication.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/SpringBootRestfulWebServicesApplication.java
@@ -3,10 +3,12 @@ package sharon.nyamongo.springbootrestfulwebservices;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootRestfulWebServicesApplication {
 
+	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
