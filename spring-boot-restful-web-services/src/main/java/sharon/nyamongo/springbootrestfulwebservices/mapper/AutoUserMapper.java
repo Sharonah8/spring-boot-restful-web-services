47ad1573354fa71f774c27428d87ffe19fe5ede2
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-18 11:03:37 +0300

    Create Mapper

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/mapper/AutoUserMapper.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/mapper/AutoUserMapper.java
@@ -1,11 +1,14 @@
 package sharon.nyamongo.springbootrestfulwebservices.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 import sharon.nyamongo.springbootrestfulwebservices.dto.UserDto;
 import sharon.nyamongo.springbootrestfulwebservices.entity.User;
 
 @Mapper
 public interface AutoUserMapper {
+
+    AutoUserMapper MAPPER = Mappers.getMapper(AutoUserMapper.class);
 
     UserDto mapToUserDto(User user);
 
