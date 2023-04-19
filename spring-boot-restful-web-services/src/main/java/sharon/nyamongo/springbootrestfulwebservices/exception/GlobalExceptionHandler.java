2a24255be57039c9865e7b767790c86bfa819b91
Sharonah8 <nyamongosharon88@gmail.com>
2023-04-19 07:34:52 +0300

    logic to implement a global exception handler

--- spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/exception/GlobalExceptionHandler.java
+++ spring-boot-restful-web-services/src/main/java/sharon/nyamongo/springbootrestfulwebservices/exception/GlobalExceptionHandler.java
@@ -34,6 +34,18 @@ public class GlobalExceptionHandler {
                 "USER_EMAIL_ALREADY_EXISTS"
         );
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(Exception exception,
+                                                                          WebRequest webRequest ){
+        ErrorDetails errorDetails = new ErrorDetails(
+                LocalDateTime.now(),
+                exception.getMessage(),
+                webRequest.getDescription(false),
+                "INTERNAL_SERVER_ERROR"
+        );
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
+
